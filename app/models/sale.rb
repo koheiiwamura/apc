d83c596f0b3026bce8093e10abe9fcb8c8fc2fb7
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-10-25 11:58:57 +0900

    tie tables with model

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,6 @@
 class Sale < ActiveRecord::Base
   self.table_name = 'sale'
+  belongs_to :user
+  belongs_to :store
+  has_many :sale_details
 end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  self.table_name = 'user_master'
+  self.primary_key = 'userid'
+  has_many :sales
+  belongs_to :store
+end
