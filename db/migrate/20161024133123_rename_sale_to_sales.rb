950b058ec9355e616c3b47735babac00e6941651
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-10-28 15:11:19 +0900

    delete-migration-file

--- db/migrate/20161024133123_rename_sale_to_sales.rb
+++
@@ -1,4 +0,0 @@
-class RenameSaleToSales < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20161024134530_drop_sale_table.rb
+++
@@ -1,5 +0,0 @@
-class DropSaleTable < ActiveRecord::Migration
-  def change
-    drop_table :sales
-  end
-end
